250e83c71be6322b74cc345f88789289d03075c2
weixiangchen520 <1981468862@qq.com>
2022-05-08 15:49:51 +0800

    feat: mysql

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -7,8 +7,18 @@ export default (appInfo: MidwayAppInfo) => {
     egg: {
       port: 7001,
     },
-    // security: {
-    //   csrf: false,
-    // },
+    security: {
+      csrf: false,
+    },
+    orm: {
+      type: 'mysql',
+      host: '182.92.83.168',
+      port: 3306,
+      username: 'root',
+      password: 'cwxwan12345',
+      database: 'portal',
+      synchronize: true, // 如果第一次使用，不存在表，有同步的需求可以写 true
+      logging: false,
+    },
   } as MidwayConfig;
 };
--- src/configuration.ts
+++ src/configuration.ts
@@ -1,11 +1,12 @@
 import { App, Configuration } from '@midwayjs/decorator';
 import { ILifeCycle } from '@midwayjs/core';
 import { Application } from 'egg';
+import * as orm from '@midwayjs/orm';
 import { join } from 'path';
 import * as egg from '@midwayjs/web';
 
 @Configuration({
-  imports: [egg],
+  imports: [egg, orm],
   importConfigs: [join(__dirname, './config')],
 })
 export class ContainerLifeCycle implements ILifeCycle {
